ed79aa223c13f64bdf3570f0116a35e14f6bf5a5
bex-cake <b.turgunov@astanait.edu.kz>
2021-05-30 09:40:51 +0600

    changes in api

--- api/models.py
+++ api/models.py
@@ -2,19 +2,9 @@ from django.db import models
 from users.models import User
 
 
-class Course(models.Model):
-    name = models.CharField(max_length=50)
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
-
-
-class CourseToGroup(models.Model):
-    groupName = models.CharField(max_length=10)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
-
 class Assignment(models.Model):
     title = models.CharField(max_length=50)
-    courseToGroup = models.ForeignKey(CourseToGroup, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
